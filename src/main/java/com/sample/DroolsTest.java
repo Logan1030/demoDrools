0faaecdca68e0627d44ff89d62fce5fd50072a17
yubin <byu@xiaohongshu.com>
2019-09-06 19:48:53 +0800

    dddd

--- src/main/java/com/sample/DroolsTest.java
+++ src/main/java/com/sample/DroolsTest.java
@@ -2,34 +2,35 @@ package com.sample;
 
  
  
+import org.drools.core.impl.InternalKnowledgeBase;
+import org.drools.core.impl.KnowledgeBaseFactory;
+import org.kie.api.KieBase;
+import org.kie.api.io.ResourceType;
+import org.kie.api.logger.KieRuntimeLogger;
+import org.kie.api.runtime.KieSession;
+import org.kie.internal.builder.KnowledgeBuilder;
+import org.kie.internal.builder.KnowledgeBuilderError;
+import org.kie.internal.builder.KnowledgeBuilderErrors;
+import org.kie.internal.builder.KnowledgeBuilderFactory;
+import org.kie.internal.io.ResourceFactory;
+import org.kie.internal.logger.KnowledgeRuntimeLoggerFactory;
+
 import java.util.ArrayList;
 import java.util.List;
-
-import org.drools.KnowledgeBase;
-import org.drools.KnowledgeBaseFactory;
-import org.drools.builder.KnowledgeBuilder;
-import org.drools.builder.KnowledgeBuilderError;
-import org.drools.builder.KnowledgeBuilderErrors;
-import org.drools.builder.KnowledgeBuilderFactory;
-import org.drools.builder.ResourceType;
-import org.drools.io.ResourceFactory;
-import org.drools.logger.KnowledgeRuntimeLogger;
-import org.drools.logger.KnowledgeRuntimeLoggerFactory;
-import org.drools.runtime.StatefulKnowledgeSession;
 
 
 public class DroolsTest {
 	public static final void main(String[] args) {  
         try {  
             // load up the knowledge base  
-            KnowledgeBase kbase = readKnowledgeBase();  
-            StatefulKnowledgeSession ksession = kbase.newStatefulKnowledgeSession();//创建会话  
-            KnowledgeRuntimeLogger logger = KnowledgeRuntimeLoggerFactory.newFileLogger(ksession, "test");  
+            KieBase kbase = readKnowledgeBase();
+            KieSession ksession = kbase.newKieSession();//创建会话
+            KieRuntimeLogger logger = KnowledgeRuntimeLoggerFactory.newFileLogger(ksession, "test");
             // go !  
             Message message = new Message();  
             message.setMessage("yubinHello World");  
             message.setStatus(Message.HELLO); 
-            List<Message> messages=new ArrayList<>();
+            List<Message> messages=new ArrayList<Message>();
             messages.add(message);
             ksession.insert(message);//插入  
             ksession.fireAllRules();//执行规则  
@@ -39,18 +40,18 @@ public class DroolsTest {
         }  
     }  
   
-    private static KnowledgeBase readKnowledgeBase() throws Exception {  
+    private static KieBase readKnowledgeBase() throws Exception {
         KnowledgeBuilder kbuilder = KnowledgeBuilderFactory.newKnowledgeBuilder();//创建规则构建器
-        kbuilder.add(ResourceFactory.newClassPathResource("sample.drl"), ResourceType.DRL);//加载规则文件，并增加到构建器  
-        KnowledgeBuilderErrors errors = kbuilder.getErrors();  
+        kbuilder.add(ResourceFactory.newClassPathResource("sample.drl"), ResourceType.DRL);//加载规则文件，并增加到构建器
+        KnowledgeBuilderErrors errors = kbuilder.getErrors();
         if (errors.size() > 0) {//编译规则过程中发现规则是否有错误  
-            for (KnowledgeBuilderError error: errors) {System.out.println("规则中存在错误，错误消息如下：");  
+            for (KnowledgeBuilderError error: errors) {System.out.println("规则中存在错误，错误消息如下：");
                 System.err.println(error);  
             }  
             throw new IllegalArgumentException("Could not parse knowledge.");  
         }  
-        KnowledgeBase kbase = KnowledgeBaseFactory.newKnowledgeBase();//创建规则构建库  
-        kbase.addKnowledgePackages(kbuilder.getKnowledgePackages());//构建器加载的资源文件包放入构建库  
+        InternalKnowledgeBase kbase = KnowledgeBaseFactory.newKnowledgeBase();//创建规则构建库
+        kbase.addPackages(kbuilder.getKnowledgePackages());//构建器加载的资源文件包放入构建库
         return kbase;  
     }  
   
--- src/main/java/com/sample/PointTest.java
+++ src/main/java/com/sample/PointTest.java
@@ -2,25 +2,28 @@ package com.sample;
 
  
  
-import org.drools.KnowledgeBase;
-import org.drools.KnowledgeBaseFactory;
-import org.drools.builder.KnowledgeBuilder;
-import org.drools.builder.KnowledgeBuilderError;
-import org.drools.builder.KnowledgeBuilderErrors;
-import org.drools.builder.KnowledgeBuilderFactory;
-import org.drools.builder.ResourceType;
-import org.drools.io.ResourceFactory;
-import org.drools.runtime.StatefulKnowledgeSession;
+
 
 import com.cn.point.PointDomain;
+import org.drools.core.impl.InternalKnowledgeBase;
+import org.drools.core.impl.KnowledgeBaseFactory;
+import org.kie.api.KieBase;
+import org.kie.api.io.ResourceType;
+import org.kie.api.runtime.KieSession;
+import org.kie.internal.builder.KnowledgeBuilder;
+import org.kie.internal.builder.KnowledgeBuilderError;
+import org.kie.internal.builder.KnowledgeBuilderErrors;
+import org.kie.internal.builder.KnowledgeBuilderFactory;
+import org.kie.internal.io.ResourceFactory;
 
 
 public class PointTest {
 	public static final void main(String[] args) {  
         try {  
             // load up the knowledge base  
-            KnowledgeBase kbase = readKnowledgeBase();  
-            StatefulKnowledgeSession ksession = kbase.newStatefulKnowledgeSession();//创建会话  
+            // load up the knowledge base
+            KieBase kbase = readKnowledgeBase();
+            KieSession ksession = kbase.newKieSession();//创建会话
              
             // go !  
             final PointDomain pointDomain = new PointDomain();
@@ -43,20 +46,20 @@ public class PointTest {
         }  
     }  
   
-    private static KnowledgeBase readKnowledgeBase() throws Exception {  
+    private static KieBase readKnowledgeBase() throws Exception {
     	System.setProperty("drools.dateformat", "yyyy-MM-dd HH:mm:ss");
         KnowledgeBuilder kbuilder = KnowledgeBuilderFactory.newKnowledgeBuilder();//创建规则构建器
-        kbuilder.add(ResourceFactory.newClassPathResource("addpoint.drl"), ResourceType.DRL);//加载规则文件，并增加到构建器  
+        kbuilder.add(ResourceFactory.newClassPathResource("addpoint.drl"), ResourceType.DRL);//加载规则文件，并增加到构建器
         kbuilder.add(ResourceFactory.newClassPathResource("subpoint.drl"), ResourceType.DRL);//加载规则文件，并增加到构建器  
-        KnowledgeBuilderErrors errors = kbuilder.getErrors();  
+        KnowledgeBuilderErrors errors = kbuilder.getErrors();
         if (errors.size() > 0) {//编译规则过程中发现规则是否有错误  
-            for (KnowledgeBuilderError error: errors) {System.out.println("规则中存在错误，错误消息如下：");  
+            for (KnowledgeBuilderError error: errors) {System.out.println("规则中存在错误，错误消息如下：");
                 System.err.println(error);  
             }  
             throw new IllegalArgumentException("Could not parse knowledge.");  
-        }  
-        KnowledgeBase kbase = KnowledgeBaseFactory.newKnowledgeBase();//创建规则构建库  
-        kbase.addKnowledgePackages(kbuilder.getKnowledgePackages());//构建器加载的资源文件包放入构建库  
+        }
+        InternalKnowledgeBase kbase = KnowledgeBaseFactory.newKnowledgeBase();//创建规则构建库
+        kbase.addPackages(kbuilder.getKnowledgePackages());//构建器加载的资源文件包放入构建库
         return kbase;  
     }  
   
